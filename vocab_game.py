a32067c153d2dbd9a0adcccca27de6e73b9b47e0
406-14-ratchapol <54373@yupparaj.ac.th>
2026-08-21 10:39:16 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,6 +101,6 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นายรัชพล กองชุมพล เลขที่ 14 ม.4/6")
 
 
